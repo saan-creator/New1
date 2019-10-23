c244264682bec30dc044659627f7d86327d8ed0d
saan-creator <36155990+saan-creator@users.noreply.github.com>
2019-10-23 17:01:38 +0530

    Update WeatherForecast.cs

--- Src/SimpleAPI/WeatherForecast.cs
+++ Src/SimpleAPI/WeatherForecast.cs
@@ -6,6 +6,7 @@ namespace SimpleAPI
     /*Comment here*/
     /*Comment here*/
     /*Comment here*/
+    /*Comment here*/
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
